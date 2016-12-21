021053ed8826a40c6132be87e72a37cd9d6ad1f7
andrey.selemenev@rtlabs.ru <andrey.selemenev@rtlabs.ru>
2016-12-21 18:28:45 +0300

    add pretty print

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,31 @@
+# -*- coding: utf-8 -*-
 import json
+from optparse import OptionParser
 
 
 def load_data(filepath):
-    pass
-
+    if not filepath:
+        raise Exception('Не указан путь к файлу')
+    try:
+        with open(filepath.strip(), 'r', encoding='utf-8') as f:
+            return f.read()
+    except FileNotFoundError:
+        print('Файл : ', filepath, 'не найден')
+    except Exception as e:
+        print('Произошла ошибка', str(e))
 
 def pretty_print_json(data):
-    pass
+    try:
+        loaded_json = json.loads(data)
+        print(json.dumps(sort_keys=True, indent=4, obj=loaded_json))
+    except TypeError:
+        print('Некорректный формат файла, файл должен содержать json структуру')
 
 
 if __name__ == '__main__':
-    pass
+    parser = OptionParser()
+    parser.add_option("-f", "--file", type="string", dest="filepath")
+    (options, args) = parser.parse_args()
+    content = load_data(options.filepath)
+    if content:
+        pretty_print_json(content)
