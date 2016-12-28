d525614e257ee82cdb5924cd8553bb75dde354b7
andrey.selemenev@rtlabs.ru <andrey.selemenev@rtlabs.ru>
2016-12-28 11:02:49 +0300

    remove Exception processing, and move json loads to load_data

--- pprint_json.py
+++ pprint_json.py
@@ -7,16 +7,13 @@ def load_data(filepath):
         raise Exception('Не указан путь к файлу')
     try:
         with open(filepath.strip(), 'r', encoding='utf-8') as f:
-            return f.read()
+            return json.loads(f.read())
     except FileNotFoundError:
         print('Файл : ', filepath, 'не найден')
-    except Exception as e:
-        print('Произошла ошибка', str(e))
 
 def pretty_print_json(data):
     try:
-        loaded_json = json.loads(data)
-        print(json.dumps(sort_keys=True, indent=4, obj=loaded_json))
+        print(json.dumps(sort_keys=True, indent=4, obj=data))
     except TypeError:
         print('Некорректный формат файла, файл должен содержать json структуру')
 
